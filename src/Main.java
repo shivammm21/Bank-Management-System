2f633ac14fdba289c5ffc70c4cfe9e4593417750
Shivam Thorat <shivamthorat00@gmail.com>
2024-01-07 15:14:34 +0530

    Added New Addition Feature DSA

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,61 @@
+
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        try {
-            Scanner scanner = new Scanner(System.in);
-            BankLogin login = new BankLogin();
-            System.out.println();
 
-            System.out.println("--------------WELCOME TO BANK MANAGEMENT SYSTEM--------------");
-            do {
-                System.out.println("1.New Account Opening");
-                System.out.println("2.Account Login");
-                System.out.println("3.Account Details");
-                System.out.println("4.Exit");
-                System.out.print("Enter number : ");
-                int choice = scanner.nextInt();
-                switch (choice) {
-                    case 1:
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        BankAcoutns obj = new BankAcoutns();
+        do{
+            System.out.println("*-----Apna-Spana New Bank-----*");
+            System.out.println("Welcome to Apna spana New bank... :)");
+            System.out.println("1.Are u New Customer ? ");
+            System.out.println("2.Our Customer ? ");
+            System.out.println("3. Admin Login ? ");
+            System.out.println("4. ? ");
+            System.out.println("5. ? ");
+            choice = sc.nextInt();
 
-                        login.newUser();
+            if(choice==1){
+                //New Customer
+                System.out.print("Your name : ");
+                String Name = sc.next();
 
-                        System.out.print("Are you login (y/n) ");
-                        char ans = scanner.next().charAt(0);
-                        ans = Character.toLowerCase(ans);
-                        if (ans == 'y') {
-                            login.newUserLogin();
+                System.out.print("Your City u Live : ");
+                String Address = sc.next();
+                System.out.print("Your Mobile Number : ");
+                String Mno = sc.next();
+                System.out.print("Create userId : ");
+                String userId = sc.next();
+                System.out.print("Create Password : ");
+                String password = sc.next();
 
-                        } else if (ans == 'n') {
-                            System.out.println("Thank you for create an Acoount");
-                        } else {
-                            System.out.println("Invalid");
-                        }
-
-                        break;
-                    case 2:
-                        login.oldUserLogin();
-                        break;
-                    case 3:
-                        login.userDetails();
-                        break;
-                    case 4:
-                        System.out.println("Thank You for Using our Services.....!!");
-                        System.exit(0);
-
-                    default:
-                        System.out.println("Invalid");
+                if(obj.insertnewData(Name,userId,password,Address,Mno)){
+                    System.out.println("Data Added Successfully..");
                 }
-            } while (true);
-        }catch (Exception e){
-            System.out.print(e);
-        }
+
+
+            }
+            else if(choice==2){
+                //Old
+                System.out.print("your Account num or userID : ");
+                String id = sc.next();
+                System.out.println("Your Password : ");
+                String pass = sc.next();
+                obj.checkLoginCustomer(id,pass);
+                    //obj.LoginCustomer();
+
+            }
+            else if(choice==3){
+                //admin
+
+
+            }
+
+
+
+        }while (choice!=10);
+
     }
 }
